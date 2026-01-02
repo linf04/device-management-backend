ac33add0a551e41fc7a89bb3661b553296b2f0f7
wanglf <wanglf@hyron.com>
2026-01-02 14:21:39 +0800

    声明接口

---
+++ src/main/java/com/device/management/repository/DevicePermissionRepository.java
@@ -0,0 +1,20 @@
+package com.device.management.repository;
+
+import com.device.management.entity.DeviceInfo;
+import com.device.management.entity.DevicePermission;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DevicePermissionRepository
+        extends JpaRepository<DevicePermission, Long>,
+        JpaSpecificationExecutor<DevicePermission>,
+        QueryByExampleExecutor<DevicePermission>
+{
+    List<DevicePermission> findDevicePermissionsByDevice(DeviceInfo device, Pageable pageable);
+}
